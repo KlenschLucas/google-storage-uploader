55799fcd1d228f1692c79f98b272ffbe37a08d8a
Klensch Lucas <klensch.lucas@interfront.local>
2018-06-28 13:47:43 +0200

    Added Torrent downloader

---
+++ torrent.js
@@ -0,0 +1,19 @@
+const WebTorrent = require('webtorrent');
+const ProgressBar = require('progress');
+
+const client = new WebTorrent();
+
+const download  = (magnetURI) => {
+  let bar = new ProgressBar('Downloading |:bar | :percent | Time Elapsed: :elapsed', { total: 100 });
+  client.add(magnetURI, { path: './downloads/' }, function (torrent) {
+    torrent.on('done', function () {
+      console.log('Torrent download finished');
+      process.exit();
+    });
+    torrent.on('download', function (bytes) {
+      bar.update(torrent.progress);
+    });
+  });
+};
+
+download('magnet:?xt=urn:btih:08ada5a7a6183aae1e09d831df6748d566095a10&dn=Sintel&tr=udp%3A%2F%2Fexplodie.org%3A6969&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969&tr=udp%3A%2F%2Ftracker.empire-js.us%3A1337&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337&tr=wss%3A%2F%2Ftracker.btorrent.xyz&tr=wss%3A%2F%2Ftracker.fastcast.nz&tr=wss%3A%2F%2Ftracker.openwebtorrent.com&ws=https%3A%2F%2Fwebtorrent.io%2Ftorrents%2F&xs=https%3A%2F%2Fwebtorrent.io%2Ftorrents%2Fsintel.torrent');
